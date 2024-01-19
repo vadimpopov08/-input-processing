935a8e478b9cebfc9b5965cee255125f5c6cc2bc
vadimpopov08 <122273142+vadimpopov08@users.noreply.github.com>
2024-01-19 16:42:50 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 const myForm = document.querySelector('#myForm')
+let name = document.getElementById('name')
+let gender = document.getElementById('gender')
 const sendButton = document.querySelector('#sendButton');
 
 sendButton.addEventListener('click', function (event) {
+    if (name.value == 'Ксения') {
+        delete name.value;
+        console.log("Вы не можете вводить имя, так так оно сильно уродливое. Измените его сначала");
+    }
 
+    else {
+        event.preventDefault();
+        console.log(myForm.elements.lastName.value);
+        console.log(myForm.elements.name.value);
+        if (myForm.elements.gender.value == 'm') {
+            console.log('Мужчина')
+        }
+        else {
+            console.log('Женщина');
+        }
+        if (myForm.elements.auto.checked) {
+            console.log('Есть автомобиль!')
+        }
+    }
 })
